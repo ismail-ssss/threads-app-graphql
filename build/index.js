035e2a6ed3d8ecf24f00f0ecba4214b19e048ab4
ismail-ssss <ibhattipuri@gmail.com>
2024-06-21 03:48:57 -0700

    prisma setup  done

--- build/index.js
+++ build/index.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const server_1 = require("@apollo/server");
 const express4_1 = require("@apollo/server/express4");
+const db_1 = require("./lib/db");
 function init() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = (0, express_1.default)();
@@ -22,16 +23,34 @@ function init() {
         app.use(express_1.default.json());
         // graphql server
         const gqlServer = new server_1.ApolloServer({
+            //schema
             typeDefs: `
         type Query{
             hello:String
             say(name:String):String
+        }
+        type Mutation{
+        createUser(firstName:String!, lastName:String!, email:String!, password:String!):Boolean
         }
     `,
             resolvers: {
                 Query: {
                     hello: () => `hey there im graphql server`,
                     say: (_, { name }) => `hey ${name}, how are you?`,
+                },
+                Mutation: {
+                    createUser: (_1, _a) => __awaiter(this, [_1, _a], void 0, function* (_, { firstName, lastName, email, password, }) {
+                        yield db_1.prismaClient.user.create({
+                            data: {
+                                email,
+                                firstName,
+                                lastName,
+                                password,
+                                salt: "random_salt",
+                            },
+                        });
+                        return true;
+                    }),
                 },
             },
         });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const app = express();
@@ -10,16 +11,48 @@ async function init() {
 
   // graphql server
   const gqlServer = new ApolloServer({
+    //schema
     typeDefs: `
         type Query{
             hello:String
             say(name:String):String
+        }
+        type Mutation{
+        createUser(firstName:String!, lastName:String!, email:String!, password:String!):Boolean
         }
     `,
     resolvers: {
       Query: {
         hello: () => `hey there im graphql server`,
         say: (_, { name }: { name: string }) => `hey ${name}, how are you?`,
+      },
+
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "random_salt",
+            },
+          });
+          return true;
+        },
       },
     },
   });
