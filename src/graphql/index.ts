24821eaaca3e6f747cb78ab68230d783a4813bb2
ismail-ssss <ibhattipuri@gmail.com>
2024-06-21 06:41:24 -0700

    added user sign up and sign in

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -5,7 +5,7 @@ async function createApolloGraphicalServer() {
   const gqlServer = new ApolloServer({
     typeDefs: `
             type Query{
-              hello: String
+              ${User.queries}
             }
             type Mutation {
               ${User.mutations}
